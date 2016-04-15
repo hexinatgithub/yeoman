2f90a0b2bbc32006f7dc26dea9550d28672c611a
Mr_he <hexin@hexindeMacBook-Pro.local>
2016-04-15 16:47:35 +0800

    website: yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mywebsiteApp
+ * @description
+ * # mywebsiteApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('mywebsiteApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.sortable'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ test/spec/controllers/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+describe('Controller: MainCtrl', function () {
+
+  it('should attach a list of awesomeThings to the scope', function () {
+    expect(true).toBe(true);
+  });
+});
